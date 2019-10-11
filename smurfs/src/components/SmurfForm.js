d9a011211ed2a7bbcfe7ccbf77c1436ad25d0a65
DimejiAre <olardi2@gmail.com>
2019-10-11 10:15:14 +0100

    create SmurfForm and export as connnected redux

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../state/actionCreators';
+
+export function SmurfForm(props) {
+    const {getFormValue, postSmurfs, smurfForm} = props;
+    
+    const submit = smurfForm => event => {
+        event.preventDefault()
+        postSmurfs(smurfForm)
+    };
+
+    return (
+        <form className='smurf-form'>
+            <label htmlFor='name'>Name</label>
+            <input value={smurfForm.name} onChange={getFormValue} name='name' type='text' />
+            <label htmlFor='age'>Age</label>
+            <input value={smurfForm.age} onChange={getFormValue} name='age' type='text' />
+            <label htmlFor='height'>height</label>
+            <input value={smurfForm.height} onChange={getFormValue} name='height' type='text' />
+            <button onClick={submit(smurfForm)}>Submit</button>
+        </form>
+    )
+}
+
+export default connect(
+    state => state,
+    actionCreators
+)(SmurfForm);
