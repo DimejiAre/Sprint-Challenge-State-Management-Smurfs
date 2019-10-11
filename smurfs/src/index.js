bf77bbec4faaf05a41b0cdece118526ae3cb42f9
DimejiAre <olardi2@gmail.com>
2019-10-11 09:01:54 +0100

    setup redux store and rootReducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { combineReducers, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import * as reducers from './state/reducers';
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootReducer = combineReducers({
+    smurfs: reducers.smurfsReducer,
+})
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    )
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById("root"));
