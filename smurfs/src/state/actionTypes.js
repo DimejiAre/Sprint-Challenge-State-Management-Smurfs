81d890369f24d80644b084484dddbb9c7e33e8ba
DimejiAre <olardi2@gmail.com>
2019-10-11 10:13:34 +0100

    create actionTypes and reducer for smurfForm

--- smurfs/src/state/actionTypes.js
+++ smurfs/src/state/actionTypes.js
@@ -1,1 +1,3 @@
 export const ADD_SMURFS = 'ADD_SMURFS';
+export const ON_INPUT_CHANGE= 'ON_INPUT_CHANGE';
+export const ON_SUBMIT = 'ON_SUBMIT';
--- smurfs/src/state/reducers.js
+++ smurfs/src/state/reducers.js
@@ -2,11 +2,26 @@ import * as types from './actionTypes';
 
 const initialSmurfs = [{ name: 'dimeji', age: 12, height: '12cm', id: 0 }]
 
+const initialSmurfForm = {name: '', age: '', height: ''}
+
 
 export function smurfsReducer(state = initialSmurfs, action) {
     switch (action.type) {
         case types.ADD_SMURFS:
             return action.payload;
+        default:
+            return state;
+    }
+}
+
+export function smurfFormReducer(state=initialSmurfForm, action){
+    switch (action.type) {
+        case types.ON_INPUT_CHANGE:
+            return {
+                ...state, [action.payload.name]: action.payload.value
+            }
+        case types.ON_SUBMIT:
+                return initialSmurfForm;
         default:
             return state;
     }
